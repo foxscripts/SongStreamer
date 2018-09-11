6d3877b9cde20a51b61342cff04f14eddb35a3cf
foxscripts <vibhormehta777@gmail.com>
2018-09-11 21:55:00 +0530

    updated code , music plays when button is clicked

--- mp3.py
+++ mp3.py
@@ -1,12 +1,31 @@
+import tkinter as tk
+# from mutagen.mp3 import MP3
 import vlc 
 import time
-from mutagen.mp3 import MP3
-
+   
 audio = "Pink Floyd - We Don't Need No Education.mp3"
-audioFile = MP3(audio)
-audio_len= audioFile.info.length
-
+# audioFile = MP3(audio)
+# audio_len= audioFile.info.length
 soundFile = vlc.MediaPlayer(audio)
-soundFile.play()
 
-time.sleep(audio_len)
+class Application(tk.Frame):
+	def __init__(self, master=None):
+		super().__init__(master)
+		self.pack()
+		self.myWindow()
+
+	def myWindow(self):
+		self.play_music = tk.Button(root, text="Play Music", command = self.play_func)
+		self.play_music.pack(side="top")
+	def play_func(self):
+		soundFile.play()
+		# time.sleep(audio_len)
+
+
+root = tk.Tk()
+root.title("Song Streamer")
+root.geometry("300x150")
+app = Application(master=root)
+app.mainloop()
+			
+
